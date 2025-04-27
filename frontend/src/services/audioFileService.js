6edcf9af7d144c0207e3bd196b9e2eb744e06025
vamshi7761 <132733844+vamshi7761@users.noreply.github.com>
2025-04-27 12:36:18 +0530

    Delete frontend directory

--- frontend/src/services/audioFileService.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const apiUrl = 'http://localhost:8080/api/music/audio-files';
-
-export const uploadAudioFile = (songId, formData) => axios.post(`${apiUrl}/upload/${songId}`, formData);
